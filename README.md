88d052a281af8aa79ed9d3d6748913269c558f67
pmb <pm.broca@gmail.com>
2020-12-23 11:18:46 +0100

    feat: capitalize option

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Sorts, cleans and capitalizes files
 
-Finds all specified files in the current directory and its sub-folders if wanted.  
-Then removes duplicated lines, capitalizes them and sort the content in a new file suffixed.  
-In the *config.properties* file, you can specify the extension looked for (default is "*txt*") and if it's recursive (default is "*true*").
+Finds all specified files in the current directory and its sub-folders if desired.  
+Then removes duplicated lines, capitalizes them and sort the content.  
+In the *config.properties* file, you can specify the extension looked for (default is "*txt*"), if it's recursive (default is "*true*") and if contents are capitalized (default is "*true*").  
+
+### How to use it
+Place the *jar* file in where the files to process are and with a command-line interface run `java -jar sort.jar`.
--- src/main/java/pmb/Sort.java
+++ src/main/java/pmb/Sort.java
@@ -21,76 +21,80 @@ import org.apache.commons.text.WordUtils;
 
 public class Sort {
 
-	private static final String PATH_ENTRY = System.getProperty("user.dir");
-	private static final String PROPERTY_KEY_EXTENSION = "extension";
-	private static final String PROPERTY_KEY_RECURSIVE = "recursive";
-	private static String extension;
-	private static Boolean recursive;
-	private static Properties prop;
+    private static final String PATH_ENTRY = System.getProperty("user.dir");
+    private static final String PROPERTY_KEY_EXTENSION = "extension";
+    private static final String PROPERTY_KEY_RECURSIVE = "recursive";
+    private static final String PROPERTY_KEY_CAPITALIZE = "capitalize";
+    private static String extension;
+    private static Boolean recursive;
+    private static Boolean capitalize;
+    private static Properties prop;
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("Start");
-		initProperties();
-		Path dir = Paths.get(PATH_ENTRY);
-		System.out.println("Start processing folder: " + dir.toFile().getAbsolutePath());
-		List<Path> files = new ArrayList<>();
-		listFilesInFolder(dir, files, extension, recursive);
-		if (files.isEmpty()) {
-			throw new RuntimeException("Folder: " + dir + " has no " + extension + " files !");
-		}
-		files.forEach(file -> {
-			try {
-				process(file);
-			} catch (UncheckedIOException e) {
-				System.out.println("Try encoding the file " + file.getFileName() + " in UTF-8");
-			}
-		});
-		System.out.println("End");
-	}
+    public static void main(String[] args) throws IOException {
+        System.out.println("Start");
+        initProperties();
+        Path dir = Paths.get(PATH_ENTRY);
+        System.out.println("Start processing folder: " + dir.toFile().getAbsolutePath());
+        List<Path> files = new ArrayList<>();
+        listFilesInFolder(dir, files, extension, recursive);
+        if (files.isEmpty()) {
+            throw new RuntimeException("Folder: " + dir + " has no " + extension + " files !");
+        }
+        files.forEach(file -> {
+            try {
+                process(file);
+            } catch (UncheckedIOException e) {
+                System.out.println("Try encoding the file " + file.getFileName() + " in UTF-8");
+            }
+        });
+        System.out.println("End");
+    }
 
-	private static void initProperties() throws IOException {
-		InputStream config = Optional.ofNullable(Sort.class.getClassLoader().getResourceAsStream("config.properties"))
-				.orElseThrow(() -> new RuntimeException("Can't find property file"));
-		prop = new Properties();
-		prop.load(config);
-		extension = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_EXTENSION)).orElse("txt");
-		recursive = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_RECURSIVE)).map(BooleanUtils::toBoolean).orElse(true);
-	}
+    private static void initProperties() throws IOException {
+        InputStream config = Optional.ofNullable(Sort.class.getClassLoader().getResourceAsStream("config.properties"))
+                .orElseThrow(() -> new RuntimeException("Can't find property file"));
+        prop = new Properties();
+        prop.load(config);
+        extension = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_EXTENSION)).orElse("txt");
+        recursive = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_RECURSIVE)).map(BooleanUtils::toBoolean).orElse(true);
+        capitalize = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_CAPITALIZE)).map(BooleanUtils::toBoolean).orElse(true);
+    }
 
-	private static void process(Path file) {
-		try (Stream<String> lines = Files.lines(file).map(WordUtils::capitalize).map(StringUtils::trim).distinct()) {
-			List<String> collect = lines.collect(Collectors.toList());
-			Collections.sort(collect, String.CASE_INSENSITIVE_ORDER);
-			Files.write(file, collect, StandardOpenOption.TRUNCATE_EXISTING);
-		} catch (IOException e) {
-			throw new RuntimeException(file.getFileName() + " failed", e);
-		}
-		System.out.println("Successfully processed file: " + file.getFileName());
-	}
+    private static void process(Path file) {
+        try (Stream<String> lines = Files.lines(file).map(line -> Boolean.TRUE.equals(capitalize) ? WordUtils.capitalize(line) : line)
+                .map(StringUtils::trim).distinct()) {
+            List<String> collect = lines.collect(Collectors.toList());
+            Collections.sort(collect, String.CASE_INSENSITIVE_ORDER);
+            Files.write(file, collect, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(file.getFileName() + " failed", e);
+        }
+        System.out.println("Successfully processed file: " + file.getFileName());
+    }
 
-	/**
-	 * Recupere la liste des fichiers d'un dossier.
-	 *
-	 * @param folder le dossier a chercher
-	 * @param files la liste qui contiendra les resultats
-	 * @param extension l'extension des fichiers a chercher
-	 * @param recursive si la recherche doit etre recursive ou non
-	 * @throws IOException
-	 */
-	public static void listFilesInFolder(final Path folder, List<Path> files, String extension, boolean recursive) throws IOException {
-		if (!Files.isDirectory(folder)) {
-			files.add(folder);
-			return;
-		}
-		try (Stream<Path> collect = Files.list(folder)) {
-			for (final Path fileEntry : collect.collect(Collectors.toList())) {
-				if (recursive && Files.isDirectory(fileEntry)) {
-					listFilesInFolder(fileEntry, files, extension, recursive);
-				} else if (StringUtils.endsWith(fileEntry.getFileName().toString(), extension)) {
-					files.add(fileEntry);
-				}
-			}
-		}
-	}
+    /**
+     * Recupere la liste des fichiers d'un dossier.
+     *
+     * @param folder le dossier a chercher
+     * @param files la liste qui contiendra les resultats
+     * @param extension l'extension des fichiers a chercher
+     * @param recursive si la recherche doit etre recursive ou non
+     * @throws IOException
+     */
+    public static void listFilesInFolder(final Path folder, List<Path> files, String extension, boolean recursive) throws IOException {
+        if (!Files.isDirectory(folder)) {
+            files.add(folder);
+            return;
+        }
+        try (Stream<Path> collect = Files.list(folder)) {
+            for (final Path fileEntry : collect.collect(Collectors.toList())) {
+                if (recursive && Files.isDirectory(fileEntry)) {
+                    listFilesInFolder(fileEntry, files, extension, recursive);
+                } else if (StringUtils.endsWith(fileEntry.getFileName().toString(), extension)) {
+                    files.add(fileEntry);
+                }
+            }
+        }
+    }
 
 }
