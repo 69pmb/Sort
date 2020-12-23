363bdc76056592ca2b7ca2461a05988535ccc546
pmb <pm.broca@gmail.com>
2020-12-23 14:36:05 +0100

    feat: gets & saves properties from input args

--- src/main/java/pmb/Sort.java
+++ src/main/java/pmb/Sort.java
@@ -1,5 +1,6 @@
 package pmb;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UncheckedIOException;
@@ -25,6 +26,7 @@ public class Sort {
     private static final String PROPERTY_KEY_EXTENSION = "extension";
     private static final String PROPERTY_KEY_RECURSIVE = "recursive";
     private static final String PROPERTY_KEY_CAPITALIZE = "capitalize";
+    private static final String CONFIG_FILE_NAME = "config.properties";
     private static String extension;
     private static Boolean recursive;
     private static Boolean capitalize;
@@ -32,7 +34,7 @@ public class Sort {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Start");
-        initProperties();
+        initProperties(args);
         Path dir = Paths.get(PATH_ENTRY);
         System.out.println("Start processing folder: " + dir.toFile().getAbsolutePath());
         List<Path> files = new ArrayList<>();
@@ -50,14 +52,41 @@ public class Sort {
         System.out.println("End");
     }
 
-    private static void initProperties() throws IOException {
-        InputStream config = Optional.ofNullable(Sort.class.getClassLoader().getResourceAsStream("config.properties"))
-                .orElseThrow(() -> new RuntimeException("Can't find property file"));
+    private static void initProperties(String[] args) throws IOException {
+        Path configPath = Path.of(CONFIG_FILE_NAME);
+        if (!configPath.toFile().exists()) {
+            InputStream config = Optional.ofNullable(Sort.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME))
+                    .orElseThrow(() -> new RuntimeException("Can't find property file"));
+            Files.copy(config, configPath);
+        }
         prop = new Properties();
-        prop.load(config);
-        extension = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_EXTENSION)).orElse("txt");
-        recursive = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_RECURSIVE)).map(BooleanUtils::toBoolean).orElse(true);
-        capitalize = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_CAPITALIZE)).map(BooleanUtils::toBoolean).orElse(true);
+        try(InputStream config = Files.newInputStream(configPath)) {
+            prop.load(config);
+            updatePropertiesFromArgs(args);
+            extension = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_EXTENSION)).orElse("txt");
+            recursive = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_RECURSIVE)).map(BooleanUtils::toBoolean)
+                    .orElse(true);
+            capitalize = Optional.ofNullable(prop.getProperty(PROPERTY_KEY_CAPITALIZE)).map(BooleanUtils::toBoolean)
+                    .orElse(true);
+        }
+    }
+
+    private static void updatePropertiesFromArgs(String[] args) {
+        if (args.length > 0 && args.length % 2 == 0) {
+            for (int i = 0; i < args.length; i += 2) {
+                String key = args[i];
+                if (List.of(PROPERTY_KEY_EXTENSION, PROPERTY_KEY_RECURSIVE, PROPERTY_KEY_CAPITALIZE).contains(key)) {
+                    prop.put(key, args[i + 1]);
+                } else {
+                    System.out.println("Property key '" + key + "' is unkown");
+                }
+            }
+        }
+        try (FileOutputStream out = new FileOutputStream(CONFIG_FILE_NAME, false)) {
+            prop.store(out, null);
+        } catch (IOException e) {
+            throw new RuntimeException("Error when saving properties", e);
+        }
     }
 
     private static void process(Path file) {
@@ -75,13 +104,14 @@ public class Sort {
     /**
      * Recupere la liste des fichiers d'un dossier.
      *
-     * @param folder le dossier a chercher
-     * @param files la liste qui contiendra les resultats
+     * @param folder    le dossier a chercher
+     * @param files     la liste qui contiendra les resultats
      * @param extension l'extension des fichiers a chercher
      * @param recursive si la recherche doit etre recursive ou non
      * @throws IOException
      */
-    public static void listFilesInFolder(final Path folder, List<Path> files, String extension, boolean recursive) throws IOException {
+    private static void listFilesInFolder(final Path folder, List<Path> files, String extension, boolean recursive)
+            throws IOException {
         if (!Files.isDirectory(folder)) {
             files.add(folder);
             return;
